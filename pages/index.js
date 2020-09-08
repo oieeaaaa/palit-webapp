7f27694abc08508176de5b0473fd2f53f41d5e4b
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-08 17:45:54 +0800

    🔥 Updated home

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,58 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Header from 'components/header/header';
+import ItemCard from 'components/itemCard/itemCard';
+import { data as cardsData } from 'components/itemCard/itemCard.styleguide';
+import Footer from 'components/footer/footer';
 import GridGuides from 'styleguide/grid-guide';
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Palit</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Header />
-    <h1>When I&#39;m home</h1>
-    <GridGuides />
-  </div>
-);
+const Home = () => {
+  const [data, setData] = useState(cardsData);
+
+  /**
+   * It toggles ❤️
+   * @param {number} id
+   */
+  const onLike = (id) => {
+    setData((prevData) => prevData.map((item) => {
+      if (item.id === id) {
+        item.is_liked = !item.is_liked;
+      }
+
+      return item;
+    }));
+  };
+
+  /**
+   * It redirects the user to the item page
+   * @param {number} id
+   */
+  const onTrade = () => {
+    // trade logic here...
+    console.log('%c Wanna trade?', 'font-size: 24px; font-family: "Avenir"'); // eslint-disable-line
+  };
+
+  return (
+    <div className="home">
+      <Head>
+        <title>Palit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <div className="grid home__list">
+        {data.map((item) => (
+          <ItemCard
+            key={item.id}
+            item={item}
+            onLike={onLike}
+            onTrade={onTrade}
+          />
+        ))}
+      </div>
+      <Footer />
+      <GridGuides />
+    </div>
+  );
+};
 
 export default Home;
--- styleguide/component-list.js
+++ styleguide/component-list.js
@@ -1,4 +1,8 @@
 // list of components
+import Footer from 'components/footer/footer.styleguide';
+
+import ItemCard from 'components/itemCard/itemCard.styleguide';
+
 import Header from 'components/header/header.styleguide';
 
 import Card from 'components/card/card.styleguide';
@@ -6,6 +10,16 @@ import Card from 'components/card/card.styleguide';
 import Button from 'components/button/button.styleguide';
 
 export default [
+  {
+    name: 'Footer',
+    component: Footer,
+  },
+
+  {
+    name: 'Item Card',
+    component: ItemCard,
+  },
+
   {
     name: 'Header',
     component: Header,
