44716c26534e497283b58db4d88a8e0b8d48924c
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-08 17:34:38 +0800

    ✨ Added footer ui

---
+++ components/footer/footer.js
@@ -0,0 +1,24 @@
+/*
+***************************************
+Component: Footer
+Author: Joimee
+Description:
+***************************************
+*/
+import { ReactSVG } from 'react-svg';
+
+export default () => (
+  <div className="footer">
+    <div className="grid">
+      <a className="footer__link --home" href="/">
+        <ReactSVG className="footer__link-icon" src="/icons/home-outline.svg" />
+      </a>
+      <a className="footer__link --trades" href="/">
+        <ReactSVG className="footer__link-icon" src="/icons/clipboard-outline.svg" />
+      </a>
+      <a className="footer__link --search" href="/">
+        <ReactSVG className="footer__link-icon" src="/icons/search-outline.svg" />
+      </a>
+    </div>
+  </div>
+);
---
+++ components/footer/footer.styleguide.js
@@ -0,0 +1,13 @@
+/*
+*******************************
+Stylguide for Footer.
+
+NOTE: Not for production use
+*******************************
+*/
+
+import Footer from './footer'
+
+export default () => (
+  <Footer />
+)
