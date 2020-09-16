a5be60fd7de574306481ca7fadc19953917b875f
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-16 22:38:58 +0800

    ✨ Added add page

---
+++ pages/items/add.js
@@ -0,0 +1,8 @@
+import ItemForm from 'components/itemForm/itemForm';
+import Layout from 'components/layout/layout';
+
+export default () => (
+  <Layout title="Add new item">
+    <ItemForm />
+  </Layout>
+);
