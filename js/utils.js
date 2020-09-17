95adf3ed758cf8d8b277920a0fac736bd3f4120c
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-17 09:46:09 +0800

    ✨ Added new util objectToArray

--- js/utils.js
+++ js/utils.js
@@ -9,6 +9,20 @@ export const extractFileURL = (file) => {
   return URL.createObjectURL(file);
 };
 
+/**
+ * objectToArray.
+ *
+ * It should convert object to array
+ * It should create a new property (key) to retain the property key/name
+ * @param {object} data
+ */
+export const objectToArray = (data) => Object.keys(data).map((key) => ({
+  ...data[key],
+  key,
+}));
+
+// export as collection of utils
 export default {
   extractFileURL,
+  objectToArray,
 };
