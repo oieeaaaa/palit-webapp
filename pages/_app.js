d595a37b0f5d4a512ccc2ac103aedac13d0b6829
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-20 13:10:19 +0800

    ✨ Lifted layout context up the component tree

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import 'scss/main.scss';
 import GridGuides from 'styleguide/grid-guide';
 import UserContext, { defaultValue as userDefaultValue } from 'js/contexts/user';
+import LayoutContext, { defaultValue as layoutDefaultValue } from 'js/contexts/layout';
 
-export default ({ Component, pageProps }) => (
-  <UserContext.Provider value={userDefaultValue}>
-    <div>
-      <Component {...pageProps} />
-      <GridGuides />
-    </div>
-  </UserContext.Provider>
-);
+export default ({ Component, pageProps }) => {
+  const [banner, setBanner] = useState(layoutDefaultValue.banner);
+
+  const layoutContextValue = {
+    banner,
+    handlers: {
+      showBanner: (data) => setBanner((prevState) => ({
+        ...prevState,
+        ...data,
+        isOpen: true,
+      })),
+      closeBanner: () => setBanner({ isOpen: false }),
+    },
+  };
+
+  return (
+    <UserContext.Provider value={userDefaultValue}>
+      <LayoutContext.Provider value={layoutContextValue}>
+        <div>
+          <Component {...pageProps} />
+          <GridGuides />
+        </div>
+      </LayoutContext.Provider>
+    </UserContext.Provider>
+  );
+};
