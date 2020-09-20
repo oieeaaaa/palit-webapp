f9cb2ff2ff7d6980af45d0eb6b929de0f2a54b82
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-20 13:09:40 +0800

    🔥 Updated layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,41 +5,23 @@ Author: Joimee
 Description:
 ***************************************
 */
-import { useState } from 'react';
 import Head from 'next/head';
-import LayoutContext, { defaultValue } from 'js/contexts/layout';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Banner from '../banner/banner';
 
-const Layout = ({ title, description, children }) => {
-  const [banner, setBanner] = useState(defaultValue.banner);
-
-  const layoutContextValue = {
-    banner,
-    handlers: {
-      showBanner: (data) => setBanner((prevState) => ({
-        ...prevState,
-        ...data,
-        isOpen: true,
-      })),
-      closeBanner: () => setBanner({ isOpen: false }),
-    },
-  };
-
-  return (
-    <LayoutContext.Provider value={layoutContextValue}>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-      </Head>
-      <Header />
-      <Banner />
-      {children}
-      <Footer />
-    </LayoutContext.Provider>
-  );
-};
+const Layout = ({ title, description, children }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Head>
+    <Header />
+    <Banner />
+    {children}
+    <Footer />
+  </>
+);
 
 Layout.defaultProps = {
   title: 'Palit',
