d81682948afa2ae06c56ff20a0020fbed85083ec
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-24 09:46:16 +0800

    ✨ End of Cycle I (The obvious)

--- styleguide/component-list.js
+++ styleguide/component-list.js
@@ -13,10 +13,6 @@ import ItemCard from 'components/itemCard/itemCard.styleguide';
 
 import Header from 'components/header/header.styleguide';
 
-import Card from 'components/card/card.styleguide';
-
-import Button from 'components/button/button.styleguide';
-
 export default [
   {
     name: 'MiniCard',
@@ -51,15 +47,5 @@ export default [
   {
     name: 'Header',
     component: Header,
-  },
-
-  {
-    name: 'Card',
-    component: Card,
-  },
-
-  {
-    name: 'Button',
-    component: Button,
   },
 ];
