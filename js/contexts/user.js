31f04c00cb7519ffb168256a53a3e73126dc39ed
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-24 09:40:57 +0800

    🔥 Updated user context default values

--- js/contexts/user.js
+++ js/contexts/user.js
@@ -1,12 +1,23 @@
 import { createContext } from 'react';
 
-export const defaultValue = {
-  id: 'v8WgF6h9IOhUHtxgrOL7mqNk8Dk1',
-  address: 'Cauayan City, Isabela',
-  email: 'joimee.cajandab@gmail.com',
-  firstName: 'Joimee',
-  lastName: 'Cajandab',
+const users = {
+  joimee: {
+    id: 'iBffuKGQ1ZUlcMrz3Orls0mH91W2',
+    address: 'Mississippi, United States',
+    email: 'joimee.cajandab@gmail.com',
+    firstName: 'Joimee',
+    lastName: 'Cajandab',
+  },
+  jm: {
+    id: 'v8WgF6h9IOhUHtxgrOL7mqNk8Dk1',
+    address: 'California, United States',
+    email: 'jm.smith@gmail.com',
+    firstName: 'JM',
+    lastName: 'Smith',
+  },
 };
+
+export const defaultValue = users.jm;
 
 const UserContext = createContext(defaultValue);
 
