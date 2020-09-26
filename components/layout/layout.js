9dc65e1e1f8cb8d16013ec42e1af215be8391a42
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-26 13:14:50 +0800

    🔥 Passed user value from UserContext to the Header component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,24 +5,30 @@ Author: Joimee
 Description:
 ***************************************
 */
+import { useContext } from 'react';
 import Head from 'next/head';
+import UserContext from 'js/contexts/user';
 import Header from '../header/header';
 import Footer, { FooterDock } from '../footer/footer';
 import Banner from '../banner/banner';
 
-const Layout = ({ title, description, children }) => (
-  <>
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-    </Head>
-    <Header />
-    <Banner />
-    {children}
-    <Footer />
-    <FooterDock />
-  </>
-);
+const Layout = ({ title, description, children }) => {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <Header user={user} />
+      <Banner />
+      {children}
+      <Footer />
+      <FooterDock />
+    </>
+  );
+};
 
 Layout.defaultProps = {
   title: 'Palit',
