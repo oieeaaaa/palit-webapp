f61041d3695f810d1edc1f7cc709aff07a6a6ab6
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-26 14:41:25 +0800

    ✨ Added header dropdown

--- components/header/header.js
+++ components/header/header.js
@@ -1,11 +1,51 @@
 /*
+eslint
+  jsx-a11y/no-noninteractive-element-interactions: 0,
+  jsx-a11y/click-events-have-key-events: 0
+*/
+
+/*
 ***************************************
 Component: Header
 Author: Joimee
 Description:
 ***************************************
+*
 */
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import useAuth from 'js/hooks/useAuth';
+
+/**
+ * HeaderDropdown.
+ *
+ * @param {object} props
+   * @param {boolean} isOpen
+   * @param {function} onLogout
+ */
+const HeaderDropdown = ({ isOpen, onLogout }) => (
+  <ul
+    className={`header-dropdown ${isOpen ? '--open' : ''}`}
+    onClick={(e) => e.stopPropagation()} // hide from window click event
+  >
+    <li className="header-dropdown__item">
+      <Link href="/profile">
+        <a className="header-dropdown__link">
+          View Profile
+        </a>
+      </Link>
+    </li>
+    <li className="header-dropdown__item">
+      <button
+        className="header-dropdown__button --logout"
+        type="button"
+        onClick={onLogout}
+      >
+        Logout
+      </button>
+    </li>
+  </ul>
+);
 
 /**
  * Header.
@@ -13,30 +53,61 @@ import Link from 'next/link';
  * @param {object} props
    * @param {object} user
  */
-const Header = ({ user }) => (
-  <div className="header">
-    <div className="grid">
-      <a className="header__brand" href="/">
-        Palit
-      </a>
+const Header = ({ user }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState();
+  const auth = useAuth();
 
-      {user.avatar && (
-        <Link href="/profile">
-          <a className="header__profile">
-            <img src="/temp/profile.png" alt="Joimee Tan Cajandab" />
-          </a>
-        </Link>
-      )}
+  /**
+   * openDropdown.
+   *
+   * @param {object} e
+   */
+  const openDropdown = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-      {(!user.avatar && user.email) && (
-        <Link href="/profile">
-          <a className="header__profile">
-            <span className="header__placeholder">{user.email[0]}</span>
-          </a>
-        </Link>
-      )}
+    setIsDropdownOpen(true);
+  };
+
+  useEffect(() => {
+    const closeDropdown = () => {
+      setIsDropdownOpen(false);
+    };
+
+    window.addEventListener('click', closeDropdown);
+
+    return () => {
+      window.removeEventListener('click', closeDropdown);
+    };
+  }, []);
+
+  return (
+    <div className="header">
+      <div className="grid">
+        <a className="header__brand" href="/">
+          Palit
+        </a>
+
+        {user.avatar && (
+          <div className="header-nav">
+            <button className="header__profile" type="button" onClick={openDropdown}>
+              <img src="/temp/profile.png" alt="Joimee Tan Cajandab" />
+            </button>
+            <HeaderDropdown isOpen={isDropdownOpen} onLogout={auth.signout} />
+          </div>
+        )}
+
+        {(!user.avatar && user.email) && (
+          <div className="header-nav">
+            <button className="header__profile" type="button" onClick={openDropdown}>
+              <span className="header__placeholder">{user.email[0]}</span>
+            </button>
+            <HeaderDropdown isOpen={isDropdownOpen} onLogout={auth.signout} />
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
