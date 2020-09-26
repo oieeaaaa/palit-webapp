d71204c93a3ebf764b54d422568d28cd454063b5
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-26 13:13:09 +0800

    🐛 Fixed header: Should only display the avatar if the user exists

--- components/header/header.js
+++ components/header/header.js
@@ -5,15 +5,36 @@ Author: Joimee
 Description:
 ***************************************
 */
-const Header = () => (
+import Link from 'next/link';
+
+/**
+ * Header.
+ *
+ * @param {object} props
+   * @param {object} user
+ */
+const Header = ({ user }) => (
   <div className="header">
     <div className="grid">
       <a className="header__brand" href="/">
         Palit
       </a>
-      <a className="header__profile" href="/">
-        <img src="/temp/profile.png" alt="Joimee Tan Cajandab" />
-      </a>
+
+      {user.avatar && (
+        <Link href="/profile">
+          <a className="header__profile">
+            <img src="/temp/profile.png" alt="Joimee Tan Cajandab" />
+          </a>
+        </Link>
+      )}
+
+      {(!user.avatar && user.email) && (
+        <Link href="/profile">
+          <a className="header__profile">
+            <span className="header__placeholder">{user.email[0]}</span>
+          </a>
+        </Link>
+      )}
     </div>
   </div>
 );
