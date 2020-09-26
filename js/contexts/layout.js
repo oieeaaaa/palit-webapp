f25eb53b74496548d0353571d6980118cc916ea1
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-09-26 13:15:59 +0800

    🔥 Extracted layout context logic from _app.js

--- js/contexts/layout.js
+++ js/contexts/layout.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { useState, createContext } from 'react';
 
 export const defaultValue = {
   banner: {
@@ -10,6 +10,34 @@ export const defaultValue = {
   },
 };
 
+// context
 const LayoutContext = createContext(defaultValue);
+
+/**
+ * LayoutProvider.
+ *
+ * @param {object} props
+   * @param {object} children
+ */
+export const LayoutProvider = ({ children }) => {
+  const [banner, setBanner] = useState(defaultValue.banner);
+  const layoutContextValue = {
+    banner,
+    handlers: {
+      showBanner: (data) => setBanner((prevState) => ({
+        ...prevState,
+        ...data,
+        isOpen: true,
+      })),
+      closeBanner: () => setBanner({ isOpen: false }),
+    },
+  };
+
+  return (
+    <LayoutContext.Provider value={layoutContextValue}>
+      {children}
+    </LayoutContext.Provider>
+  );
+};
 
 export default LayoutContext;
