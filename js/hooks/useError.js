146b72affcfa40a5c2b0582b2caa0a4f5dfde885
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-10-01 05:55:42 +0800

    🔥 Updated useError hook

--- js/hooks/useError.js
+++ js/hooks/useError.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import LayouContext from 'js/contexts/layout';
 
 /**
@@ -7,6 +7,7 @@ import LayouContext from 'js/contexts/layout';
  * It display an error message to the user
  */
 const useError = () => {
+  const [error, setError] = useState(null);
   const { handlers } = useContext(LayouContext);
 
   /**
@@ -15,17 +16,20 @@ const useError = () => {
    * It should display the error.message
    * @param {object} error
    */
-  const displayError = (error) => {
-    console.error(error.message);
+  const displayError = (err) => {
+    console.error(err.message);
 
     handlers.showBanner({
       text: error.message,
       variant: 'error',
     });
+
+    setError(error);
   };
 
   return [
     displayError,
+    error,
   ];
 };
 
