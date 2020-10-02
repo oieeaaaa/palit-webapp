188b533106b006dd716c9193c5a60c7b84c95bc7
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-10-02 18:53:57 +0800

    📝 Another text edit

--- components/tradeRequest/tradeRequest.js
+++ components/tradeRequest/tradeRequest.js
@@ -180,7 +180,7 @@ export const TradeRequestItemRequests = ({
 export const TradeRequestEmpty = ({ isEmpty }) => isEmpty && (
   <div className="tip">
     <h2 className="tip-heading">
-      No traded items yet:
+      Empty:
     </h2>
     <p className="tip-text">
       Try to make a
