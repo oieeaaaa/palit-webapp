463d38280d9ea2912ce013e7027ace87f9401b17
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-10-11 22:37:24 +0800

    📝 Text Edits: Revised good luck note

--- components/goodLuckCard/goodLuckCard.js
+++ components/goodLuckCard/goodLuckCard.js
@@ -7,15 +7,32 @@ Description:
 */
 import Link from 'next/link';
 
+/**
+ * GoodLuckCard.
+ *
+ * @param {object}
+   * @param {string} ownerID
+ */
 const GoodLuckCard = ({ ownerID }) => (
   <div className="goodLuckCard">
-    <h2 className="goodLuckCard__title">Best of luck!</h2>
+    <h2 className="goodLuckCard__title">
+      Yay! Your item is traded, Congrats!
+      <span role="img" aria-label="Yehey!"> 🎉</span>
+    </h2>
     <p className="goodLuckCard__text">
-      We&lsquo;ve sent an email to the traded Item&lsquo;s Owner.
+      We&lsquo;ve sent an email to the owner of the Accepted Item.
     </p>
     <p className="goodLuckCard__text">
-      But if you want to do it your way, See the contact info. of the  Owner
-      and send them a message.
+      For now this is as far as our service can help you trade your item the
+      rest is up to you to figure out how are you going to make the trade
+      happen in real life.
+    </p>
+    <p className="goodLuckCard__text">
+      Please checkout the Owner&apos;s profile to get some more information about the
+      owner of the accepted item.
+    </p>
+    <p className="goodLuckCard__text">
+      Be NICE. We wish you the best!
     </p>
     <Link href="/profile/[ownerID]" as={`/profile/${ownerID}`}>
       <a className="button --primary">
