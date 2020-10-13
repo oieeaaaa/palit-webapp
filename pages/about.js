5021334c166223e91591a0a421c513388aab63c7
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-10-13 20:39:10 +0800

    🔥 Cleanup: About page

--- pages/about.js
+++ pages/about.js
@@ -1,39 +1,50 @@
+import { useContext } from 'react';
+import AuthContext from 'js/contexts/auth';
+import Link from 'next/link';
 import { LandingFooter } from 'components/landing/landing';
 import Header from 'components/header/header';
 
-const About = () => (
-  <div className="about">
-    <Header />
-    <div className="grid">
-      <figure className="about__image">
-        <img src="/images/palit.png" alt="Palit" />
-      </figure>
-      <h1 className="about__heading">What is palit?</h1>
-      <div className="about__content">
-        <p className="about__text">
-          Palit is a trading platform for people who need to trade their items fast
-        </p>
-        <form
-          className="about__donate"
-          action="https://www.paypal.com/cgi-bin/webscr"
-          method="post"
-          target="_top"
-        >
-          <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="CSAUGRJNW949A" />
-          <button
-            name="submit"
-            className="button --primary"
-            type="submit"
-            title="PayPal - The safer, easier way to pay online!"
+const About = () => {
+  const { isVerified } = useContext(AuthContext);
+
+  return (
+    <div className="about">
+      <Header />
+      <div className="grid">
+        <figure className="about__image">
+          <img src="/images/palit.png" alt="Palit" />
+        </figure>
+        <h1 className="about__heading">What is palit?</h1>
+        <div className="about__content">
+          <p className="about__text">
+            Palit is a trading platform for people who need to trade items.
+            {' '}
+            {!isVerified && (
+              <Link href="/signup"><a className="about__link">I want to experience palit</a></Link>
+            )}
+          </p>
+          <form
+            className="about__donate"
+            action="https://www.paypal.com/cgi-bin/webscr"
+            method="post"
+            target="_top"
           >
-            Send us some love
-          </button>
-        </form>
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input type="hidden" name="hosted_button_id" value="CSAUGRJNW949A" />
+            <button
+              name="submit"
+              className="button --primary"
+              type="submit"
+              title="PayPal - The safer, easier way to pay online!"
+            >
+              Send us some love
+            </button>
+          </form>
+        </div>
       </div>
+      <LandingFooter />
     </div>
-    <LandingFooter />
-  </div>
-);
+  );
+};
 
 export default About;
