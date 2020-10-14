d31561507d6f93b9659ce18e8649a04b8673c7e3
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-10-14 19:20:33 +0800

    ✨ Added new footer info (I have a problem)

--- js/routes.js
+++ js/routes.js
@@ -11,7 +11,26 @@ export const routes = [
   },
 ];
 
+export const publicRoutes = [
+  {
+    name: 'signup',
+    href: '/signup',
+  },
+  {
+    name: 'login',
+    href: '/login',
+  },
+  {
+    name: 'about',
+    href: '/about',
+  },
+];
+
 export const footerInfo = [
+  {
+    name: 'I have a problem',
+    href: '/report-a-problem',
+  },
   {
     name: 'About',
     href: '/about',
