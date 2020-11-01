72f9d75d17330e1dedd29a1752f7dd128311895d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-01 13:17:51 +0800

    🐛 Used the proper way to import admin

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
-import admin from 'firebase-admin';
 import serviceAccount from './palit-fb2a5-firebase-adminsdk-i87c4-7f92ea78ec.json';
 
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
 const config = {
@@ -14,4 +14,4 @@ const app = !admin.apps.length ? admin.initializeApp(config) : admin.app();
 export const db = app.firestore();
 export const fn = functions;
 
-export default app;
+export default admin;
