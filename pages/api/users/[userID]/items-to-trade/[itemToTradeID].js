e5a16dd61c4015212ac24cb8fdc05cc50ef03fbb
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-02 06:52:15 +0800

    🚚 Moved item to trade api

---
+++ pages/api/users/[userID]/items-to-trade/[itemToTradeID].js
@@ -0,0 +1,54 @@
+import { db } from 'admin';
+import { normalizeData } from 'js/utils';
+import {
+  successPayload,
+  errorPayload,
+} from 'js/shapes/commons';
+import api from 'js/helpers/api';
+
+const itemsCollection = db.collection('items');
+const tradeRequestsCollection = db.collection('tradeRequests');
+
+/**
+ * getItemsToTrade.
+ *
+ * @param {object} req
+ * @param {object} res
+ */
+const getItemsToTrade = async (req, res) => {
+  const { userID, itemToTradeID } = req.query;
+
+  try {
+    const rawItems = await itemsCollection.where('owner', '==', userID).where('isTraded', '==', false).get();
+    const newItems = [];
+
+    for (const rawItem of rawItems.docs) {
+      let isItemAlreadyTraded = false;
+      const rawRequests = await tradeRequestsCollection.doc(itemToTradeID).collection('requests').get();
+
+      if (!rawRequests.empty) {
+        for (const rawRequest of rawRequests.docs) {
+          if (rawRequest.id === rawItem.id) {
+            isItemAlreadyTraded = true;
+          }
+        }
+      }
+
+      if (!isItemAlreadyTraded) {
+        newItems.push(normalizeData(rawItem));
+      }
+    }
+
+    successPayload(res, {
+      data: newItems,
+      message: 'Good luck trading!',
+    });
+  } catch (error) {
+    errorPayload(res, {
+      message: 'Ooops, Something went wrong.',
+      error: error.message,
+    });
+  }
+};
+
+export default api({ get: getItemsToTrade });
